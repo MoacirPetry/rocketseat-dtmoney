5fc7732c8a03a6d8031ba83df9dee932ec119273
Moacir Petry <m.petry9@gmail.com>
2021-09-21 15:06:38 -0300

    feat: add google font-family - poppins

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,13 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 400;
+  }
   html {
     @media (max-width: 1080px) {
       font-size: 93.75%;
