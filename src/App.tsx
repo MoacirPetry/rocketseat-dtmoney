46c91139fb2fb2c3a8fdbd0d49aac664c303c48f
Moacir Petry <m.petry9@gmail.com>
2021-09-28 17:50:34 -0300

    feat: add hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsProvider } from './TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root');
 export function App() {
